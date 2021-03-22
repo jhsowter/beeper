9404004cb9f1bda5ca9839fa14919c7cba20ccae
Jack Sowter <jhsowter@gmail.com>
2021-03-22 19:00:56 +1100

    some basic wave stuff

--- beeper/src/Lib.hs
+++ beeper/src/Lib.hs
@@ -1,6 +1,20 @@
 module Lib
-    ( someFunc
+    ( 
+        someFunc,
+        wave
     ) where
+
+-- audible spectrum
+lowHz = 20
+highHz = 20000
+
+hz :: Float
+hz = 10000
 
 someFunc :: IO ()
 someFunc = putStrLn "weelo hold"
+
+sound = wave 0.01 0.5
+
+wave :: Float -> Float -> [Float]
+wave pitchF volume = map (*volume) $ map sin $ map (*pitchF) [0..hz]
