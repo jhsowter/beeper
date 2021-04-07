a24b9cdf413e5780309b57e663f92a6936e3729f
Jack Sowter <jhsowter@gmail.com>
2021-04-08 09:08:42 +1000

    make a low, choppy beep

--- beeper/src/Lib.hs
+++ beeper/src/Lib.hs
@@ -3,10 +3,11 @@ module Lib
     ( 
         someFunc,
         wave,
-        sound
+        sound,
+        toInt16
     ) where
 
-import Control.Monad (unless)
+import Control.Monad (unless, forM_)
 import SDL (openAudioDevice, Changeable(Mandate), pollEvents, EventPayload(..))
 import qualified SDL
 import  Data.Vector.Storable.Mutable (IOVector)
@@ -47,14 +48,14 @@ loop = do
   let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
   unless quit loop
           
-
-sound = wave 0.01 0.5
+sound :: [Int16]
+sound = map toInt16 $ map (*4800) $ wave 0.01 0.5
 
 wave :: Float -> Float -> [Float]
 wave pitchF volume = map (*volume) $ map sin $ map (*pitchF) [0..hz]
 
--- toInt16 :: [Float] -> [Int16]
--- toInt16 f = fromIntegral $ floor f 
+toInt16 :: Float -> Int16
+toInt16 f = fromIntegral $ floor f 
 
 --openAudioDevice :: MonadIO m => OpenDeviceSpec -> m (AudioDevice, AudioSpec)
 
@@ -63,5 +64,10 @@ audioCallback format buffer = do
   case format of
     SDL.Signed16BitLEAudio -> do -- witness sampleType ~ Int16
       Vector.set buffer 0 -- safety silence
+      let size = Vector.length buffer
+      let z = zip [0..size-1] sound
+      forM_ z $ \(i, value) -> do
+        putStrLn $ show (i, value)
+        Vector.write buffer i value
       -- TODO: write your 16-bit ints of samples here 
     _ -> error "Unsupported audio format."
