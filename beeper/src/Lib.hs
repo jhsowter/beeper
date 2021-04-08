9e53907b117d2521ea5f3f10b09cf604bda401b9
Jack Sowter <jhsowter@gmail.com>
2021-04-08 18:57:57 +1000

    Make sign wave infinite

--- beeper/src/Lib.hs
+++ beeper/src/Lib.hs
@@ -13,15 +13,17 @@ import qualified SDL
 import  Data.Vector.Storable.Mutable (IOVector)
 import qualified Data.Vector.Storable.Mutable as Vector
 import Data.Int (Int16)
+import Data.IORef (newIORef, readIORef, writeIORef, IORef(..))
 -- audible spectrum
 lowHz = 20
-highHz = 20000
+highHz = 15000 -- 20k is audible but..
 
 hz :: Float
-hz = 10000
+hz = 15000
 
 someFunc :: IO ()
 someFunc = do
+    counter <- newIORef 0
     SDL.initializeAll
     window <- SDL.createWindow "Beeper" SDL.defaultWindow {
       SDL.windowInitialSize = SDL.V2 1280 720
@@ -31,7 +33,7 @@ someFunc = do
         SDL.openDeviceFormat = Mandate SDL.Signed16BitLEAudio,
         SDL.openDeviceChannels = Mandate SDL.Stereo,
         SDL.openDeviceSamples = 4096,
-        SDL.openDeviceCallback = audioCallback,
+        SDL.openDeviceCallback = audioCallback counter,
         SDL.openDeviceUsage = SDL.ForPlayback,
         SDL.openDeviceName = Nothing
     }
@@ -49,25 +51,31 @@ loop = do
   unless quit loop
           
 sound :: [Int16]
-sound = map toInt16 $ map (*4800) $ wave 0.01 0.5
+sound = map toInt16 $ wave 1000 440
 
 wave :: Float -> Float -> [Float]
-wave pitchF volume = map (*volume) $ map sin $ map (*pitchF) [0..hz]
+wave amp freq
+  | freq > lowHz && freq < highHz = map (*amp) $ map sin $ map (*freq) [0,1..]
+  | otherwise = error "freq outside audible range"
 
 toInt16 :: Float -> Int16
-toInt16 f = fromIntegral $ floor f 
+toInt16 f = fromIntegral $ floor f
 
 --openAudioDevice :: MonadIO m => OpenDeviceSpec -> m (AudioDevice, AudioSpec)
 
-audioCallback :: SDL.AudioFormat sampleType -> IOVector sampleType -> IO ()
-audioCallback format buffer = do
+audioCallback :: IORef Int -> SDL.AudioFormat sampleType -> IOVector sampleType -> IO ()
+audioCallback counterRef format buffer = do
   case format of
     SDL.Signed16BitLEAudio -> do -- witness sampleType ~ Int16
+      putStrLn "callback"
+      counter <- readIORef counterRef
       Vector.set buffer 0 -- safety silence
       let size = Vector.length buffer
-      let z = zip [0..size-1] sound
-      forM_ z $ \(i, value) -> do
-        putStrLn $ show (i, value)
-        Vector.write buffer i value
+      let values = zip [0..size-1] $ drop counter sound
+      forM_ values $ \(index, value) -> do
+        --Vector.write buffer index value
+        print (index, value, counter)
       -- TODO: write your 16-bit ints of samples here 
+
+      writeIORef counterRef (counter+size)
     _ -> error "Unsupported audio format."
